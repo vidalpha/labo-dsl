fb7d5a1f7aa60438a900f4f7e9cf9426a56a81e5
AZONHOUMON Vidjinnangni Alphonse Ignace <azonhoumonignace@gmail.com>
2025-01-02 11:49:46 +0100

    Init projet gradle avec kotlin DSL

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+// The pluginManagement block must appear before any other statements in the script.
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "DSL"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
